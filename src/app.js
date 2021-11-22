2df0dc657dfdb9b53e2c6c367c21476a8009358f
YOUR NAME <aetchevers@eagerworks.com>
2021-11-22 17:58:41 -0300

    Get tests all working, added two endponints for post

--- src/app.js
+++ src/app.js
@@ -1,39 +1,55 @@
 var express = require('express');
 var cors = require('cors');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 var app = express();
 
+app.use(cookieParser());
 app.use(cors({
     origin: '*'
 }));
+app.use(express.urlencoded({
+  extended: true
+}));
+
+var experiencia_laboral = {
+  "experiencia-laboral": [
+    {
+      "empresa": "Agencia del BSE",
+      "puesto": "Area de Comercio",
+      "descripcion": "Agencia de Seguros del BSE. Trabaje en el area de comercio y comunicación con el cliente.",
+      "fechaInicio": new Date("01/03/2018"),
+      "fechaFin": new Date("10/02/2021")
+    },
+    {
+      "empresa": "Parroquia Stella Maris - Movimiento Sembradores",
+      "puesto": "Acción Social - Ayudante",
+      "descripcion": "Ayudante en el movimiento Sembradores. Es un grupo de personas las cuales le dan de comer y le brindan ayuda a gente con situación de calle",
+      "fechaInicio": new Date("30/06/2019"),
+      "fechaFin": new Date("21/12/2019")
+    },
+    {
+      "empresa": "EagerWorks",
+      "puesto": "Fullstack Developer",
+      "descripcion": "EagerWorks es una Software Factory especializada en desarrollo web y mobile para iOS y Android. Desarrolladora Junior Fullstack de Aplicaciones Web utilizando Ruby on Rails para su implementacion.",
+      "fechaInicio": new Date("21/04/2021"),
+      "fechaFin": null
+    }
+  ]
+}
 
 app.get('/experiencia-laboral', function(req, res) {
-  experiencia_laboral = {
-    "experiencia_laboral": [
-      {
-        "empresa": "Agencia del BSE",
-        "puesto": "Area de Comercio",
-        "descripcion": "Agencia de Seguros del BSE. Trabaje en el area de comercio y comunicación con el cliente.",
-        "fechaInicio": "01-03-2018",
-        "fechaFin": "10-02-2021"
-      },
-      {
-        "empresa": "Parroquia Stella Maris - Movimiento Sembradores",
-        "puesto": "Acción Social - Ayudante",
-        "descripcion": "Ayudante en el movimiento Sembradores. Es un grupo de personas las cuales le dan de comer y le brindan ayuda a gente con situación de calle",
-        "fechaInicio": "30-06-2019",
-        "fechaFin": "21-12-2019"
-      },
-      {
-        "empresa": "EagerWorks",
-        "puesto": "Fullstack Developer",
-        "descripcion": "EagerWorks es una Software Factory especializada en desarrollo web y mobile para iOS y Android. Desarrolladora Junior Fullstack de Aplicaciones Web utilizando Ruby on Rails para su implementacion.",
-        "fechaInicio": "21-04-2021",
-        "fechaFin": "Presente"
-      }
-    ]
-  }
   res.send(experiencia_laboral);
+});
+
+var jsonParser = bodyParser.json();
+
+app.post('/enviar-formulario', jsonParser, function(req, res) {
+});
+
+app.post("/*", jsonParser, function(req, res) {
+  res.status(404).send("404 - No fue encontrado");
 });
 
 app.listen(process.env.PORT || 3000, (a) => {
